5c1045e2fc726aee8d3bd8e4c0a35e26bf82b8b9
ItzNinja228 <datuna.israelashvili@gmail.com>
2026-02-18 11:26:27 +0200

    feat(client): defined most nonogram endpoints

--- apps/client/src/store/api/nonogram-api.ts
+++ apps/client/src/store/api/nonogram-api.ts
@@ -1,5 +1,62 @@
+import { Nonogram } from './../../../../server/src/modules/nonogram/entity/nonogram.entity';
+import {
+  CreateNonogramRequestDto,
+  GenerateNonogramDto,
+  GeneratedNonogramResponseDto,
+} from '@nonogram-api-monorepo/types';
 import { api } from './api';
 
 export const nonogramApi = api.injectEndpoints({
-  endpoints: (build) => ({}),
+  endpoints: (build) => ({
+    generateNonogram: build.query<
+      GeneratedNonogramResponseDto,
+      GenerateNonogramDto
+    >({
+      query: (body) => ({
+        url: 'nonogram/generate',
+        method: 'POST',
+        body: body,
+      }),
+    }),
+    createNonogram: build.query<
+      void /*TODO need to add logic for clean object return so user wont access the solved nonogram */,
+      CreateNonogramRequestDto
+    >({
+      query: (body) => ({
+        url: 'nonogram/create',
+        method: 'POST',
+        body: body,
+      }),
+    }),
+    getNonogramLeaders: build.query<Nonogram[] /* here as well */, void>({
+      query: (nonogramId) => ({
+        url: `nonogram/leaders/${nonogramId}`,
+        method: 'GET',
+      }),
+    }),
+    getGlobalLeaders: build.query<void /*TODO!!!*/, void>({
+      query: () => ({
+        url: 'nonogram/global-leaders',
+        method: 'GET',
+      }),
+    }),
+    getAllAvaliableNonograms: build.query<Nonogram[], void>({
+      query: (userId) => ({
+        url: `nonogram/all/${userId}`,
+        method: 'GET',
+      }),
+    }),
+    getUnplayedNonograms: build.query<Nonogram[], void>({
+      query: (userId) => ({
+        url: `nonogram/unplayed/${userId}`,
+        method: 'GET',
+      }),
+    }),
+    getNonogram: build.query<Nonogram, void>({
+      query: (nonogramId) => ({
+        url: `nonogram/${nonogramId}`,
+        method: 'GET',
+      }),
+    }),
+  }),
 });
