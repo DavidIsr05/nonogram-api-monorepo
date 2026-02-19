494a07a2d8f37e600c9db69569e978c4a9896e86
ItzNinja228 <datuna.israelashvili@gmail.com>
2026-02-19 13:53:58 +0200

    feat(client): added svgs and defined some file structure

--- apps/client/src/main.tsx
+++ apps/client/src/main.tsx
@@ -1,15 +1,15 @@
 import { StrictMode } from 'react';
 import * as ReactDOM from 'react-dom/client';
-import { App } from './app/app';
 import { Provider } from 'react-redux';
 import { store } from './store/store';
 import { createBrowserRouter, RouterProvider } from 'react-router-dom';
+import { Landing } from './routes/landing';
 
 const root = ReactDOM.createRoot(
   document.getElementById('root') as HTMLElement
 );
 
-const router = createBrowserRouter([{ path: '/*', element: <App /> }]);
+const router = createBrowserRouter([{ path: '/*', element: <Landing /> }]);
 
 root.render(
   <StrictMode>
